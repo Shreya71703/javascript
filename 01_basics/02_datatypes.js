e84b2fa9aa20d503a707a36df462c293b1bcb08f
Shreya Shukla <shreyashukl3@gmail.com>
2025-09-11 17:25:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"user strict"; //treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs not browser
+
+console.log(3 + 3); // code readability should be high
+
+//https://tc39.es/ecma262/2023/ for javaScript specifications
+
+let name = "Shreya"; // string literal
+let age = 20; // number literal
+let isApproved = false; // boolean literal
+let State = undefined; // empty value
+
+//number => 2 to power 53
+//bigint => larger than 2 to power 53
+//string => '',"",``
+//boolean => true/false
+//null => standalone value
+//undefined => variable not defined
+//symbol => unique identifiers
+
+//object => key-value pairs
+
+console.log(typeof null); //it shows object but it should be null
+console.log(typeof undefined); // it shows undefined
