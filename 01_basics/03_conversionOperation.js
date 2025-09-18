f24ebb6aa9f5141951f0091a5fe40a951e6bc437
Shreya Shukla <shreyashukl3@gmail.com>
2025-09-18 04:06:19 +0000

    datatypes summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,22 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "shreya"
+let str2 = "ben"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log("1" + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
