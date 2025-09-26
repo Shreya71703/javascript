0b732857204084bcbe82cec97e255090318829bd
Shreya Shukla <shreyashukl3@gmail.com>
2025-09-26 18:47:09 +0000

    truthy.js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,38 @@
+const userEmail = "shreya@gmail.com"
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("No user email");
+}
+
+// Falsy values: false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy values: "0", 'false', " ", [], {}, function(){}, etc.
+
+if(userEmail.length === 0) {
+    console.log("Array is empty");
+}
+
+const emptyObject = {}
+
+if(Object.keys(emptyObject).length === 0) {
+    console.log("Object is empty");
+}
+
+//  Nullish Coalescing Operator (??): null undefined
+
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10
+val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+console.log(val1); 
+
+// Ternary Operator
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
